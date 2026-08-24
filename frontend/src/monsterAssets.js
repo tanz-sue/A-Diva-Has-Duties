b47537af9309807e4ca972020a941e1d2537cf7d
Tamanna <tamannajana06@gmail.com>
2026-08-25 02:20:50 +0800

    pray this works hiw

--- frontend/src/monsterAssets.js
+++ frontend/src/monsterAssets.js
@@ -3,7 +3,7 @@ import deadlineWraith from "./assests/monsters/deadline_wraith.jpg"
 import clutterGolem from "./assests/monsters/clutter_golem.jpg";
 import inboxHydra from "./assests/monsters/inbox_hydra.jpg";
 
-export const MONSTER_IMAGES ={
+export const MONSTER_IMAGES = {
     beetle_bug : beetleBug,
     deadline_wraith : deadlineWraith,
     clutter_golem : clutterGolem,
--- frontend/src/warriorAssets.js
+++ frontend/src/warriorAssets.js
@@ -2,7 +2,7 @@ import witchCat from "./assests/warriors/witch_cat.jpg";
 import raccoonBaker from "./assests/warriors/raccoon_baker.jpg";
 import penguinWizard from "./assests/warriors/penguin_wizard.jpg";
 
-export const WARRRIOR_IMAGES = {
+export const WARRIOR_IMAGES = {
     witch_cat: witchCat,
     raccoon_baker: raccoonBaker,
     penguin_wizard: penguinWizard,
